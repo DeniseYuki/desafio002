9856205aa7c2c37a81315bc4e189c76b666467db
DeniseYuki <49623035+DeniseYuki@users.noreply.github.com>
2021-08-23 17:38:04 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -9,32 +9,34 @@ namespace desafio001
         {
             Console.WriteLine("quantos elementos além dos 2 iniciais vc quer? ");
             int quantidade = int.Parse(Console.ReadLine());
-            //var idade = int.Parse(Console.ReadLine());
+            string nome;
 
-            // for (int i = 0; i < idade; i++)
-            // {
-            //     Console.WriteLine("nOME");
-            //     Console.WriteLine("Idade" + (40 + i));
-            // }
+            Console.WriteLine("Digite seu nome: ");
+            nome = Console.ReadLine();
 
-            //-------
+
             var dic = new Dictionary<string, int>();
-            dic.Add("Nome1", 10);
-            dic.Add("Nome2", 20);
+            dic.Add($"Nome1: {nome}", 10);
+            Console.WriteLine("Digite seu nome: ");
+            nome = Console.ReadLine();
+
+            dic.Add($"Nome2: {nome}", 20);
             for (int i = 0; i < quantidade; i++)
             {
                 var a = i + 3;
-                dic.Add($"Nome{a}", (40 + i));
+                string nomeA;
+                Console.WriteLine("Digite seu nome: ");
+                nomeA = Console.ReadLine();
+                dic.Add($"Nome{a}:{nomeA}", (40 + i));
 
 
             }
-                foreach (var item in dic)
-                {
-                    Console.WriteLine(item.Key + " tem " + item.Value + " anos");
-                }
+            foreach (var item in dic)
+            {
+                Console.WriteLine(item.Key + " tem " + item.Value + " anos");
             }
-
-            //dic.Add("Nome" + i, (40 + i));
         }
-    }
 
+        
+    }
+}
