3c31a88dc0f7788c6690ee593f7673d5eb1dd9b8
Dev <Dev@DESKTOP-HP1V55E>
2021-08-23 13:15:48 -0300

    Adicionar arquivos de projeto.

---
+++ desafio001/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace desafio001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int id = 0;
+            String nome;
+            int idade;
+            String cidade;
+            int continuar;
+
+            id = 0;
+            Console.WriteLine("ID: " + id);
+
+            Console.WriteLine("Digite seu nome");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Digite sua idade: ");
+            idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o nome da sua cidade: ");
+            cidade = Console.ReadLine();
+
+
+            Console.WriteLine("Deseja continuar cadastrando? [0/1]");
+            continuar = int.Parse(Console.ReadLine());
+
+            while (continuar == 0)
+                // id++;
+                //Console.WriteLine(id);
+                if (continuar == 0)
+                {
+                    id++;
+                    Console.WriteLine("ID: " + id);
+
+                    Console.WriteLine("Digite seu nome");
+                    nome = Console.ReadLine();
+
+                    Console.WriteLine("Digite sua idade: ");
+                    idade = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Digite o nome da sua cidade: ");
+                    cidade = Console.ReadLine();
+                    //continue;
+                    Console.WriteLine("Deseja continuar cadastrando? [0/1]");
+                    continuar = int.Parse(Console.ReadLine());
+
+
+                    if (continuar == 1)
+                        break;
+                    //Console.Write("Cadastro Finzalizado!!");
+                }
+            Console.Write("Cadastro Finzalizado!!");
+
+        }
+    }
+}
