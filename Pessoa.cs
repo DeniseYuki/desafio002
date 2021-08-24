64db207d260cad80aff379632cb25d63f45cdabf
DeniseYuki <49623035+DeniseYuki@users.noreply.github.com>
2021-08-24 14:11:16 -0300

    Add files via upload

---
+++ Pessoa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desafio002
+{
+    public class Pessoa
+    {
+        //- nome
+        public string Nome { get; set; }
+        //- Idade
+        public int Idade { get; set; }
+        //- Data de nascimento
+        public int DataDeNascimento { get; set; }
+        //- Sexo(Masc/Fem)
+        public string Sexo { get; set; }
+        //- RA
+        public int RA { get; set; }
+        //- CPF
+        public int Cpf { get; set; }
+        //- Formado(sim ou não)
+        public string Formado { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Desafio002;
+using System;
 using System.Collections.Generic;
 
 namespace desafio001
@@ -9,34 +10,49 @@ namespace desafio001
         {
             Console.WriteLine("quantos elementos além dos 2 iniciais vc quer? ");
             int quantidade = int.Parse(Console.ReadLine());
-            string nome;
+            //var idade = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Digite seu nome: ");
-            nome = Console.ReadLine();
+            // for (int i = 0; i < idade; i++)
+            // {
+            //     Console.WriteLine("nOME");
+            //     Console.WriteLine("Idade" + (40 + i));
+            // }
+
+            //-------
+            var p = new Pessoa();
+            p.Nome = "joão";
+            p.Idade = 12;
+            p.DataDeNascimento = 03/08/1990;
+            p.Cpf = 123456789;
+            p.RA = 17024269;
+            p.Sexo = "masculino";
+            p.Formado = "sim";
+
+            var listP = new List<Pessoa>();
+            listP.Add(p);
+
+            foreach (var Cadastro in listP)
+                Console.WriteLine(Cadastro.Nome + Cadastro.RA) ;
 
 
-            var dic = new Dictionary<string, int>();
-            dic.Add($"Nome1: {nome}", 10);
-            Console.WriteLine("Digite seu nome: ");
-            nome = Console.ReadLine();
-
-            dic.Add($"Nome2: {nome}", 20);
             for (int i = 0; i < quantidade; i++)
             {
+                var p2 = new Pessoa();
+                p2.Nome = $"joão {i}";
+                p2.Idade =10+ i;
+
+
                 var a = i + 3;
-                string nomeA;
-                Console.WriteLine("Digite seu nome: ");
-                nomeA = Console.ReadLine();
-                dic.Add($"Nome{a}:{nomeA}", (40 + i));
+                listP.Add(p2);
 
 
             }
-            foreach (var item in dic)
+            foreach (var aluno in listP)
             {
-                Console.WriteLine(item.Key + " tem " + item.Value + " anos");
+                Console.WriteLine(aluno.Nome + " tem " + aluno.Idade + " anos");
             }
         }
 
-        
+        //dic.Add("Nome" + i, (40 + i));
     }
 }
