934be0198d526e265f4bf741ee71149f3911a617
DeniseYuki <49623035+DeniseYuki@users.noreply.github.com>
2021-08-26 14:54:16 -0300

    Add files via upload

--- Pessoa.cs
+++ Pessoa.cs
@@ -10,16 +10,35 @@ namespace Desafio002
         public string Nome { get; set; }
         //- Idade
         public int Idade { get; set; }
-        //- Data de nascimento
-        public int DataDeNascimento { get; set; }
-        //- Sexo(Masc/Fem)
-        public string Sexo { get; set; }
+
+        public DateTime DataNascimento { get; set;}
+
+        //public bool SexoGenero { get; set;}
+        public string SexoGenero(bool Genero){
+            Genero = true;
+            string textoReturn = "";
+            if (Genero)
+            {
+                textoReturn += "Masculino";
+            }
+
+            return textoReturn;
+        }
+
+
+
+
+
         //- RA
         public int RA { get; set; }
         //- CPF
-        public int Cpf { get; set; }
+        public string Cpf { get; set; }
         //- Formado(sim ou não)
         public string Formado { get; set; }
+
+    }
+    public class IdadeNascimento
+    {
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,37 +6,35 @@ namespace desafio001
 {
     class Program
     {
+        public static object Cadastro { get; private set; }
+
         static void Main(string[] args)
         {
+
             Console.WriteLine("quantos elementos além dos 2 iniciais vc quer? ");
             int quantidade = int.Parse(Console.ReadLine());
-            //var idade = int.Parse(Console.ReadLine());
-
-            // for (int i = 0; i < idade; i++)
-            // {
-            //     Console.WriteLine("nOME");
-            //     Console.WriteLine("Idade" + (40 + i));
-            // }
-
-            //-------
+           
             var p = new Pessoa();
-            p.Nome = "joão";
-            p.Idade = 12;
-            //arrumar a data de nascimento
-            p.DataDeNascimento = 03/08/1990;
-            // Cpf é string 
-            p.Cpf = 123456789;
-            p.RA = 17024269;
-            // bool 
-            p.Sexo = "masculino";
-            p.Formado = "sim";
+                p.Nome = "joão";
+                p.Idade = 12;
+           // Pessoa IdadeNascimento = new Pessoa();
+                p.DataNascimento = new DateTime(1990, 08, 02);
+
+                p.Cpf = "123456789";
+
+                p.RA = 17024269;
+
+                //p.SexoGenero(true);
+
+                Console.WriteLine(p.SexoGenero(true));
+                p.Formado = "sim";
 
             var listP = new List<Pessoa>();
             listP.Add(p);
 
             foreach (var Cadastro in listP)
-                Console.WriteLine(Cadastro.Nome + Cadastro.RA) ;
-
+                Console.WriteLine(Cadastro.Nome + Cadastro.RA + Cadastro.DataNascimento); //Cadastro.SexoGenero) ;
+                //Console.WriteLine(SexoGenero);
 
             for (int i = 0; i < quantidade; i++)
             {
@@ -52,9 +50,15 @@ namespace desafio001
             }
             foreach (var aluno in listP)
             {
+                Console.WriteLine(p.SexoGenero(true));
                 Console.WriteLine(aluno.Nome + " tem " + aluno.Idade + " anos");
+                //return textoReturn;
             }
         }
+
+
+
+
 
         //dic.Add("Nome" + i, (40 + i));
     }
